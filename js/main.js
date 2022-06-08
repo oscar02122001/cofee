f6ea38609a8046ebf28481bbb3bac6a6e0c6c420
oscar02122001 <olimjonismatov01@gmail.com>
2022-06-08 23:08:04 +0500

    coffe full

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector(".header__burger");
+const nav = document.querySelector(".header__nav")
+
+burger.addEventListener("click", function(){
+    burger.classList.toggle("header__burger--opened")
+    nav.classList.toggle("header__nav--opened")
+})
